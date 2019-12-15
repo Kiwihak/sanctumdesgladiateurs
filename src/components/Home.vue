9efbbff61d05ad1b85c3c56427252c0392569f3a
Kiwihak <58908949+Kiwihak@users.noreply.github.com>
2019-12-15 18:28:51 +0100

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <div class="container-fluid">
+  <div class="row">
+    <div class="col">
+      Accueil + Le Fonctionnement
+    </div>
+    <div class="col">
+      Barre de recherche
+    </div>
+    <div class="col">
+      Se Connecter
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    props: {
+
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #section-home {
+    height: 580px;
+    padding-top: 50px;
+  }
+  #logo {
+    height: 100px;
+  }
+</style>
